4026cfd4a42d74e62c345f3ddec3e9515204fd3f
StreamMao <maosijun1995@gmail.com>
2020-04-23 16:59:12 -0400

    Fetch from server and Handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes';
+import { baseUrl } from '../shared/baseUrl';
 
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
@@ -11,13 +12,28 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 });
 
-//Thunk
-export const fetchDishes = () =>(dispatch) => {
+//Thunk Dishes
+export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
-    
-    setTimeout(() => {
-        dispatch(addDishes(DISHES))
-    }, 2000);
+    return fetch(baseUrl + 'dishes')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                //There is a reponse of error from server
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
+                throw error;
+            }
+        },
+        //Don't hear back anything from server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(dishes => dispatch(addDishes(dishes)))
+        .catch(error => dispatch(dishesFailed(error.message))); // this will catch both
 }
 
 //Actions
@@ -33,4 +49,79 @@ export const dishesFailed = (errmess) => ({
 export const addDishes = (dishes) =>({
     type: ActionTypes.ADD_DISHES,
     payload: dishes
-})
+});
+
+//Thunk Comments
+export const fetchComments = () =>(dispatch) => {
+    return fetch(baseUrl + 'comments')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                //There is a reponse of error from server
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
+                throw error;
+            }
+        },
+        //Don't hear back anything from server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(comments => dispatch(addComments(comments)))
+        .catch(error => dispatch(commentsFailed(error.message))); // this will catch both
+
+        
+}
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) =>({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+//Thunk Promos
+export const fetchPromos = () =>(dispatch) => {
+    dispatch(promosLoading(true));
+    
+    return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if (response.ok) {
+                return response;
+            } else {
+                //There is a reponse of error from server
+                var error = new Error('Error ' + response.status + ': ' + response.statusText)
+                error.response = response;
+                throw error;
+            }
+        },
+        //Don't hear back anything from server
+        error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+        })
+        .then(response => response.json())
+        .then(promos => dispatch(addPromos(promos)))
+        .catch(error => dispatch(promosFailed(error.message))); // this will catch both
+}
+
+//Actions
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) =>({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
+});
